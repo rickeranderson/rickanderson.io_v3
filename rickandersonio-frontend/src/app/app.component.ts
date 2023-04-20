a039a18af23f295e50cbedbbac61ace395714d52
Rick Anderson <richard.anderson@ranova.io>
2023-04-20 14:28:23 -0600

    Updated main page to use profile object

--- rickandersonio-frontend/src/app/app.component.ts
+++ rickandersonio-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Profile } from './models/profile.model';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,85 @@ export class AppComponent {
   title = 'RickAnderson.io';
 
   date = new Date();
+
+  profile: Profile;
+
+  constructor() { }
+
+  ngOnInit(){
+    let languageSkills = {
+      name: 'Languages',
+      items: [
+        'Java',
+        'Beanshell', 
+        'TypeScript', 
+        'Javascript', 
+        'C#', 
+        'PHP', 
+        'Python', 
+        'HTML', 
+        'CSS', 
+        'C', 
+        'SQL'
+      ]
+    }
+
+    let techSkills = {
+      name: 'Technologies/Frameworks',
+      items: [
+        'SailPoint IdentityIQ', 
+        'Windows', 
+        'Linux', 
+        'macOS', 
+        'Angular', 
+        'ASP.Net', 
+        '.Net Core', 
+        'Git', 
+        'VisualStudio', 
+        'VSCode',
+        'Microsoft IIS', 
+        'Azure DevOps', 
+        'Apache Tomcat',
+        'Octopus Deploy',
+        'JetBrains TeamCity'
+      ]
+    }
+
+    let awsExperience = {
+      name: 'AWS Experience',
+      items: [
+        'EC2',
+        'Lambda', 
+        'Elastic Beanstalk', 
+        'S3', 
+        'RDS', 
+        'CloudFront', 
+        'Route53', 
+        'API Gateway', 
+        'CloudWatch',
+        'CloudFormation', 
+        'IAM', 
+        'SNS', 
+        'SES'
+      ]
+    }
+
+    this.profile = {
+      name: 'Rick Anderson',
+      education: [
+        '2017 - Computer Science, B.S. - Boise State University'
+      ],
+      employment: [
+        '2022 - Present, Software Engineer III, J.R. Simplot Company',
+        '2021 - 2022, Software Engineer II, J.R. Simplot Company',
+        '2017 - 2021, Software Engineer, Boise State University - Enterprise Application Development',
+        '2016 - 2017, Student Developer, Boise State University'
+      ],
+      skills: [
+        languageSkills,
+        techSkills,
+        awsExperience
+      ]
+    }
+  }
 }
---
+++ rickandersonio-frontend/src/app/models/profile.model.ts
@@ -0,0 +1,11 @@
+export class Profile {
+    name: string;
+    education: string[];
+    employment: string [];
+    skills: Skill[];
+}
+
+export class Skill {
+    name: string;
+    items: string[];
+}
